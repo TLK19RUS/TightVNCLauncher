2efa14dfbec24de169f919656f7d3b6b4782c832
Ivanov Ivan <tlk19rus@aol.com>
2020-11-03 14:03:42 +0700

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TightVNCLauncher
+{
+    //tvnviewer -host="192.168.15.1" -password="Yflt;lf0"
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
